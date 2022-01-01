b904e661afcb8e4355ef5595d40a4a74811d4e78
Avatarlol <BuriedAliveZX@Gmail.com>
2022-01-02 01:12:08 +0200

    project progress

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,59 @@
-import './Header.css';
+import classes from './Header.css';
+import Login from '../login/Login'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import userEvent from '@testing-library/user-event';
 
 export default function Header() {
 
     const user = {
         name: 'bruh',
-        type: 'CUSTOMER',
+        type: 'GUEST',
     }
 
+    const openLoginForm = () => {
+
+        console.log("logging in...");
+
+        return (
+            <Login />
+        )
+    };
+
+
+    const [isLoginForm, setLoginForm] = useState(false)
+
+    const openLoginFormHandler = () => {
+        setLoginForm(true);
+    }
+
+    const logoutHandler = () => {
+        console.log('logging off...');
+    }
 
     return (
-        <div className='header'>
-            
-        <div id='logo'>Logo</div>
-        <div className='menu'>home</div>
+        <React.Fragment>
+            {isLoginForm && openLoginForm()}
 
-        <div id='login'>
-            Welcome, {<br/>}{user.type=='GUEST' ? 'guest' : user.name}.
-            <br/><br/>
-            <button> {user.type=='GUEST' ? 'Log-in' : 'Log-out'}</button>
-        </div>
+            <div className='header'>
 
-        </div>
+                <div id='logo'>Logo</div>
+
+                <div className='menu'>home</div>
+
+
+
+                <div id='login'>
+                    Welcome, {<br />}{user.type == 'GUEST' ? 'guest' : user.name}.
+                    <br /><br />
+                    <button
+                        onClick={user.type == 'GUEST' ? openLoginFormHandler : logoutHandler}>
+                        {user.type == 'GUEST' ? 'Log-in' : 'Log-out'}
+                    </button>
+                </div>
+
+            </div>
+
+        </React.Fragment>
     )
 }
---
+++ src/components/login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Login.css'
+
+export default function Login() {
+    return (
+        <div className='background'>
+            <div className='form'></div>
+        </div> 
+    )
+}
